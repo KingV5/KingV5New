d60c33619b0859400338a268ed241b0bcf520f83
KingV5 <jeremiahelpro@gmail.com>
2026-03-10 10:41:33 +0000

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,54 @@
+local Arguments = ... or {}
+if not Arguments.Key then
+    Arguments.Key = script_key or 'unknown key'
+end
+
+local _, subbed = pcall(function()
+    return game:HttpGet('https://github.com/MaxlaserTech/CatV6')
+end)
+
+local commit = subbed:find('currentOid')
+commit = commit and subbed:sub(commit + 13, commit + 52) or nil
+commit = commit and #commit == 40 and commit or 'main'
+Arguments.Commit = commit
+
+if shared.VapeDeveloper then
+    return loadstring(readfile('catrewrite/loader.lua'), 'loader.lua')(Arguments)
+else
+    if not isfolder('catrewrite') then
+        makefolder('catrewrite')
+    end
+
+    if not isfolder('catrewrite/profiles') then
+        makefolder('catrewrite/profiles')
+    end
+
+    local function downloadFile(path, func)
+        if not isfile(path) or (not isfile('catrewrite/profiles/commit.txt') or readfile('catrewrite/profiles/commit.txt') ~= commit) and not shared.VapeDeveloper then
+            local suc, res = pcall(function()
+                return game:HttpGet('https://raw.githubusercontent.com/MaxlaserTech/CatV6/'.. commit.. '/' ..select(1, path:gsub('catrewrite/', '')), true)
+            end)
+            if not suc or res == '404: Not Found' then
+                error(res)
+            end
+            if path:find('.lua') then
+                res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+            end
+            writefile(path, res)
+        end
+        return (func or readfile)(path)
+    end
+
+    if getconnections and not shared.catdebug then
+        for _, v in getconnections(cloneref(game:GetService('ScriptContext')).Error) do
+            v:Disable()
+        end
+
+        for _, v in getconnections(cloneref(game:GetService('LogService')).MessageOut) do
+            v:Disable()
+        end
+    end
+
+    shared.VapeDeveloper = Arguments.Developer
+    return loadstring(downloadFile('catrewrite/loader.lua'), 'loader.lua')(Arguments)
+end
